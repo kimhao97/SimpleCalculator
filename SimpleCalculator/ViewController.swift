ec17b66c3c02d086085a962ac4f1747af5e0ec66
kimhao97 <nguyenkimhaodn@gmail.com>
2021-01-25 08:58:49 +0700

    clean code

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -1,31 +1,17 @@
-//
-//  ViewController.swift
-//  SimpleCalculator
-//
-//  Created by Hao Kim on 1/21/21.
-//
-
 import UIKit
 
 enum Operator {
     case add, sub, mul, div, percent, nav
 }
 
-//enum Clear {
-//    case none, clear
-//
-//    var isClear = {
-//
-//    }
-//}
-
 class ViewController: UIViewController {
-    //MARK: - Properties
-    private var numbers:[Float] = []
-    private var operators:[Operator] = []
+    @IBOutlet weak var doingLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    private var numbers: [Float] = []
+    private var operators: [Operator] = []
     private var isDecimalPoint = false
     private var isClear = false
-    
     private var index: Int = 0 {
         didSet {
             isDecimalPoint = false
@@ -44,22 +30,22 @@ class ViewController: UIViewController {
             if doingString.first == "0" {
                 doingString.removeAll()
             }
+            
             doingString.append(numberString)
             
             if numbers.isEmpty {
                 numbers.append(Float(numberString) ?? 0)
-            }else {
+            } else {
                 if(numbers.count == index) {
                     numbers.append(0)
                 }
                 
                 if isDecimalPoint {
-                    numbers[index] = numbers[index] + (Float(numberString) ?? 0)/10
+                    numbers[index] = numbers[index] + (Float(numberString) ?? 0) / 10
                 }else {
-                    numbers[index] = numbers[index]*10 + (Float(numberString) ?? 0 )
+                    numbers[index] = numbers[index] * 10 + (Float(numberString) ?? 0 )
                 }
             }
-            print("index: \(index), count: \(numbers.count), number: \(numbers)")
             numberString.removeAll()
         }
     }
@@ -91,9 +77,11 @@ class ViewController: UIViewController {
             default:
                 break
             }
+            
             if operatorString != "." {
                 index += 1
             }
+            
             doingString.append(contentsOf: " " + operatorString + " ")
             operatorString.removeAll()
         }
@@ -104,6 +92,7 @@ class ViewController: UIViewController {
             doingLabel.text = doingString
         }
     }
+    
     private var resultString: String = "" {
         didSet {
             let result: Double = Double(resultString) ?? 0
@@ -116,16 +105,13 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var doingLabel: UILabel!
-    @IBOutlet weak var resultLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
     }
     
     //MARK: - Clear action
-    @IBAction func clearTouchUpInside(_ sender: Any) {
+    @IBAction private func clearTouchUpInside(_ sender: Any) {
         guard doingString.isEmpty  else {
             numbers.removeAll()
             doingString.removeAll()
@@ -135,32 +121,39 @@ class ViewController: UIViewController {
     }
     
     //MARK: - Arithmetic operators action
-    @IBAction func addTouchUpInside(_ sender: Any) {
+    @IBAction private func addTouchUpInside(_ sender: Any) {
         operatorString.append("+")
     }
-    @IBAction func subTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func subTouchUpInside(_ sender: Any) {
         operatorString.append("-")
     }
-    @IBAction func mulTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func mulTouchUpInside(_ sender: Any) {
         operatorString.append("x")
     }
-    @IBAction func divTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func divTouchUpInside(_ sender: Any) {
         operatorString.append("/")
     }
-    @IBAction func percentTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func percentTouchUpInside(_ sender: Any) {
         operatorString.append("%")
     }
-    @IBAction func negativeTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func negativeTouchUpInside(_ sender: Any) {
         operatorString.append("+/-")
     }
-    @IBAction func dotTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func dotTouchUpInside(_ sender: Any) {
         operatorString.append(".")
     }
-    @IBAction func equalTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func equalTouchUpInside(_ sender: Any) {
         isClear = true
         if index == 0 {
             resultString = "\(numbers.first ?? 0)"
-        }else {
+        } else {
             // percent
             var i: Int = 0
             repeat {
@@ -196,7 +189,7 @@ class ViewController: UIViewController {
                     }
                 } while i < index
             }
-
+            
             
             // add, sub
             if index != 0 {
@@ -224,35 +217,42 @@ class ViewController: UIViewController {
     }
     
     //MARK: - Number action
-    @IBAction func zeroTouchUpInside(_ sender: Any) {
+    @IBAction private func zeroTouchUpInside(_ sender: Any) {
         numberString.append("0")
     }
-    @IBAction func oneTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func oneTouchUpInside(_ sender: Any) {
         numberString.append("1")
     }
-    @IBAction func twoTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func twoTouchUpInside(_ sender: Any) {
         numberString.append("2")
     }
-    @IBAction func threeTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func threeTouchUpInside(_ sender: Any) {
         numberString.append("3")
     }
-    @IBAction func fourTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func fourTouchUpInside(_ sender: Any) {
         numberString.append("4")
     }
-    @IBAction func fiveTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func fiveTouchUpInside(_ sender: Any) {
         numberString.append("5")
     }
-    @IBAction func sixTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func sixTouchUpInside(_ sender: Any) {
         numberString.append("6")
     }
-    @IBAction func sevenTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func sevenTouchUpInside(_ sender: Any) {
         numberString.append("7")
     }
-    @IBAction func eightTouchUpInside(_ sender: Any) {
+    @IBAction private func eightTouchUpInside(_ sender: Any) {
         numberString.append("8")
     }
-    @IBAction func nineTouchUpInside(_ sender: Any) {
+    
+    @IBAction private func nineTouchUpInside(_ sender: Any) {
         numberString.append("9")
     }
 }
-
